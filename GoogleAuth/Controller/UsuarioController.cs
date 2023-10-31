a33270390d83eb8acce88821cefe098cbdd924a4
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2023-10-31 02:29:14 -0300

    teste4

--- GoogleAuth/Controller/UsuarioController.cs
+++ GoogleAuth/Controller/UsuarioController.cs
@@ -27,10 +27,10 @@ public class UsuarioController : ControllerBase
         //return Ok("Sucesso");
     }
 
-    [HttpGet("/teste")]
+    [HttpGet("{id:int}")]
     public ActionResult Get2()
     {
-        return Content("Success");
+        return Ok("Success");
     }
 
 
