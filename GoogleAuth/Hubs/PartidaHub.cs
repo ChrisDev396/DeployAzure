d9e0e1fd655539fcff8ac9595986442317587f65
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2023-11-01 00:48:56 -0300

    hub

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -4,26 +4,41 @@ using Microsoft.AspNetCore.SignalR;
 [Authorize]
 public class PartidaHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    private static int maxUsersInRoom = 2;
+    private static int usersInRoom = 0;
+
+    public async Task JoinRoom(string roomName)//roomName
     {
-
-        //if (message == "salaCriada")
-        //{
-            // await Clients.Client(user).SendAsync("SendMessage", user, message);
-            await Clients.All.SendAsync("SendMessage", user, message);
-        //}
-        //else
-        //{
-
-        //}
+        //await Clients.Client(user).SendAsync("SendMessage", user, message);
+        //await Clients.All.SendAsync("SendMessage", user, message);
         
+        if (usersInRoom < maxUsersInRoom)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            usersInRoom++;
+
+            // Informe ao usuário que eles entraram na sala.
+            await Clients.Caller.SendAsync("RoomJoined", roomName);
+        }
+        else
+        {
+            // A sala está cheia. Você pode informar ao usuário ou tomar outra ação apropriada.
+            await Clients.Caller.SendAsync("RoomFull");
+        }
     }
+
+    public async Task SendMessageToRoom(string roomName, string user, string message)
+    {
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        // Ao desconectar, reduza o contador de usuários na sala.
+        usersInRoom--;
+
+        await base.OnDisconnectedAsync(exception);
+    }
+        
+    
 }
-
-
-
-//public async IAsyncEnumerable<DateTime> Straming(CancellationToken cancellationToken)
-//{
-//    yield return DateTime.Now;
-//    await Task.Delay(1000, cancellationToken);
-//}
