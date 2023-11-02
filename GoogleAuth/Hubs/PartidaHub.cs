8f4c7d2f41d6704a495383de22a16d1f86052ddc
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2023-11-01 22:35:21 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -9,22 +9,19 @@ public class PartidaHub : Hub
 
     public async Task JoinRoom(string roomName)//roomName
     {
-        while (true)
+        if (usersInRoom < maxUsersInRoom)
         {
-            if (usersInRoom < maxUsersInRoom)
-            {
-                //await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
-                usersInRoom++;
-                roomName += roomName;
-                // Informe ao usuário que eles entraram na sala.
-                await Clients.Caller.SendAsync("RoomJoined", roomName);
-            }
-            else
-            {
-                usersInRoom++;
-                // A sala está cheia. Você pode informar ao usuário ou tomar outra ação apropriada.
-                await Clients.Caller.SendAsync("RoomJoined", usersInRoom.ToString());
-            }
+            //await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            usersInRoom++;
+            roomName += roomName;
+            // Informe ao usuário que eles entraram na sala.
+            await Clients.Caller.SendAsync("RoomJoined", roomName);
+        }
+        else
+        {
+            usersInRoom++;
+            // A sala está cheia. Você pode informar ao usuário ou tomar outra ação apropriada.
+            await Clients.Caller.SendAsync("RoomJoined", usersInRoom.ToString());
         }
     }
 
