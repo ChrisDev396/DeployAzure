f19f07cec01e78cef75e577ed8eec0123bb4edfc
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-20 00:14:50 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -1,5 +1,6 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Threading;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json.Linq;
@@ -10,48 +11,57 @@ public class PartidaHub : Hub
     private static int maxUsersInRoom = 2;
     private static int usersInRoom = 0;
     private static string sala = "";
+    private static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+    //private static List<string> Salas = new List<string>();
+
 
     public async Task JoinRoom()
     {
-        var claims = Context.User.Claims;
+        await _semaphore.WaitAsync();
 
-        var emailClaim = claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email);
-
-        if (usersInRoom < maxUsersInRoom)
+        try
         {
-            if (emailClaim != null)
-            {
-                string email = emailClaim.Value;
+            var claims = Context.User.Claims;
+            var emailClaim = claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email);
 
-                if (email.Equals(sala))
+            if (usersInRoom < maxUsersInRoom)
+            {
+                if (emailClaim != null)
                 {
-                    sala = "bloqueado";
+                    string email = emailClaim.Value;
+                    usersInRoom++;
+                    sala += email + "/";
                 }
-                sala += email;
             }
-      
-            //await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
-            
-            usersInRoom++;
-        }
-        else
-        {
-            sala = "";
-
-            if (emailClaim != null)
+            else
             {
-                string email = emailClaim.Value;
-                sala += email;
+                sala = "";
+                if (emailClaim != null)
+                {
+                    string email = emailClaim.Value;
+                    sala += email + "/";
+                }
+                usersInRoom = 1;
             }
-            usersInRoom = 1;
+            await Clients.All.SendAsync("JoinRoom", sala);
         }
-        await Clients.All.SendAsync("JoinRoom", sala);
+        finally
+        {
+            _semaphore.Release();
+        }
     }
 
-    public async Task SendMessageToRoom(string roomName, string user, Jogador jogador)
+    public async Task SendMessageToRoom(string roomName, string mensagem)
     {
-        
-        await Clients.Group(roomName).SendAsync("acao", user);
+        var claims = Context.User.Claims;
+        var emailClaim = claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email);
+
+        string[] nomes = roomName.TrimEnd('/').Split('/');
+
+        if (nomes[0] == emailClaim.Value || nomes[1] == emailClaim.Value)
+        {
+            await Clients.Group(roomName).SendAsync("mensagem", mensagem);
+        }
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
@@ -62,5 +72,10 @@ public class PartidaHub : Hub
         await base.OnDisconnectedAsync(exception);
     }
 
-
+    /*
+                if (email.Equals(sala))
+                {
+                    sala = "bloqueado";
+                }
+    */
 }
