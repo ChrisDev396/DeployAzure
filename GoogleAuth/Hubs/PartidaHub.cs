1ff5a21c8dba829a93c9a8dffa6f6939e263a37f
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-21 04:44:58 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -52,7 +52,7 @@ public class PartidaHub : Hub
 
     public async Task SendMessageToRoom(string roomName)
     {
-        await Clients.All.SendAsync("SendMessageToRoom", "teste");
+        await Clients.Group(roomName).SendAsync("SendMessageToRoom", "teste");
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
