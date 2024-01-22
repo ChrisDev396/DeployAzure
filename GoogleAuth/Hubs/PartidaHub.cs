b848f30a851338ef32b6f5d3650a35736509ed89
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-22 02:44:46 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Threading;
@@ -12,34 +13,42 @@ public class PartidaHub : Hub
     private static int usersInRoom = 0;
     private static int sala = 1;
     private static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
-    private static List<string> salas;
-    private static string nomeSala = "";
 
-    public async Task JoinRoom(string baralho)
+    private static string[] baralhos;
+    private Dictionary<string, string[]> dictionary = new Dictionary<string, string[]>();
+
+    public async Task JoinRoom(string[] baralho)
     {
         await _semaphore.WaitAsync();
 
         try
         {
             //Thread.Sleep(10000);
-            //var claims = Context.User.Claims;
-            //var emailClaim = claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email).Value;
+            var claims = Context.User.Claims;
+            var emailClaim = claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email).Value;
 
-            //if (emailClaim != null)
-            //{
-            //    nomeSala += emailClaim;
-            //}
+            if (emailClaim != null)
+            {
+            }
 
             usersInRoom++;
 
+            baralhos = new string[] { emailClaim };
+            baralhos.Concat(baralho);
+
+
             await Groups.AddToGroupAsync(Context.ConnectionId, sala.ToString());
 
+            
 
             if (usersInRoom == 2)
             {
-                await Clients.Group(sala.ToString()).SendAsync("JoinRoom", sala.ToString());
+                dictionary.Add(sala.ToString(), baralhos);
+                await Clients.Group(sala.ToString()).SendAsync("JoinRoom", $"{baralhos[0]} {baralhos[1]} {baralhos[2]} {baralhos[3]}");
+                
                 sala++;
                 usersInRoom = 0;
+                Array.Clear(baralhos, 0, baralhos.Length);
             }
 
         }
@@ -60,5 +69,13 @@ public class PartidaHub : Hub
 
         await base.OnDisconnectedAsync(exception);
     }
-
+//    if (jogadores.ContainsKey(chaveASerRemovida))
+//{
+//    jogadores.Remove(chaveASerRemovida);
+//    Console.WriteLine($"Chave {chaveASerRemovida} removida do cache.");
+//}
+//else
+//{
+//    Console.WriteLine($"Chave {chaveASerRemovida} não encontrada no cache.");
+//}
 }
