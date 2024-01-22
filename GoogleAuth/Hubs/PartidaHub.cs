387af1366d47588fd8c8936f5a79da64b449e05f
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-22 05:43:32 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Threading;
@@ -14,13 +15,16 @@ public class PartidaHub : Hub
     private static int sala = 1;
     private static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
-    private static string[] jogador1;
-    private static string[] jogador2;
-    private Dictionary<string, string[]> dictionary = new Dictionary<string, string[]>();
+    private static Jogador jogador1;
+    private static Jogador jogador2;
 
-    private static List<string> list = new List<string>();
+    private Dictionary<string, List<Jogador>> dictionary = new Dictionary<string, List<Jogador>>();
 
-    public async Task JoinRoom(string combatente, string carta1, string carta2, string carta3, string carta4, string carta5)
+    private static List<Jogador> list = new List<Jogador>();
+
+    private static bool valorAleatorio;
+
+    public async Task JoinRoom(string[] baralho)
     {
         await _semaphore.WaitAsync();
 
@@ -38,12 +42,11 @@ public class PartidaHub : Hub
             //baralhos = new string[] { emailClaim ,baralho};
             //baralhos.Concat(baralho);
 
-            //list.Add(emailClaim);
-            
-            //foreach (string valor in baralho)
-            //{
-            //    list.Add(valor);
-            //}
+
+            foreach (string valor in baralho)
+            {
+                
+            }
 
 
             await Groups.AddToGroupAsync(Context.ConnectionId, sala.ToString());
@@ -52,17 +55,25 @@ public class PartidaHub : Hub
 
             if (usersInRoom == 2)
             {
-                jogador2 = new string[] { emailClaim, combatente, carta1, carta2, carta3, carta4, carta5 };
-                jogador1.Concat(jogador2);
-                await Clients.Group(sala.ToString()).SendAsync("JoinRoom", $"Testes{jogador1[0]}");
-                dictionary.Add(sala.ToString(), jogador1);
+                valorAleatorio = !valorAleatorio;
+                jogador2 = new Jogador(emailClaim, baralho, valorAleatorio);
+                
+                await Clients.Group(sala.ToString()).SendAsync("JoinRoom", sala.ToString(),valorAleatorio);
+
+                list.Add(jogador1);
+                list.Add(jogador2);
+                dictionary.Add(sala.ToString(), list);
+
                 sala++;
                 usersInRoom = 0;
-                //Array.Clear(baralhos, 0, baralhos.Length);
+                
+               
             }
             else
             {
-                jogador1 = new string[] { emailClaim, combatente, carta1, carta2, carta3, carta4, carta5 };
+                valorAleatorio = new Random().Next(2) == 0;
+                jogador1 = new Jogador(emailClaim, baralho, valorAleatorio);
+                
             }
 
         }
@@ -75,7 +86,13 @@ public class PartidaHub : Hub
 
     public async Task SendMessageToRoom(string roomName)
     {
-        await Clients.Group(roomName).SendAsync("SendMessageToRoom", "teste");
+        if (dictionary.ContainsKey(roomName))
+        {
+            
+        }
+
+        await Clients.Group(roomName).SendAsync("SendMessageToRoom", "testeeee");
+
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
