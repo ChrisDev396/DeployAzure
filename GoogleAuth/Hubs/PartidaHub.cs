9aba566d2acc33779ff949ecd5c554ff075e9b71
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-23 05:33:21 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -44,24 +44,27 @@ public class PartidaHub : Hub
 
             if (usersInRoom == 2)
             {
-                valorAleatorio = !valorAleatorio;
-                jogador2 = new Jogador(Context.ConnectionId.ToString(), baralho, valorAleatorio, sala.ToString());
+                //valorAleatorio = !valorAleatorio;
+                //jogador2 = new Jogador(Context.ConnectionId.ToString(), baralho, valorAleatorio, sala.ToString());
 
-                list.Add(jogador1);
-                list.Add(jogador2);
-                dictionary.Add(sala.ToString(), list);
-                list.Clear();
+                //list.Add(jogador1);
+                //list.Add(jogador2);
+                //dictionary.Add(sala.ToString(), list);
+                //list.Clear();
+
                 await Clients.Group(sala.ToString()).SendAsync("JoinRoom", sala.ToString());
+
                 sala++;
                 usersInRoom = 0;
                 
             }
             else
             {
-                valorAleatorio = new Random().Next(2) == 0;
-                jogador1 = new Jogador(Context.ConnectionId.ToString(), baralho, valorAleatorio, sala.ToString());
+                //valorAleatorio = new Random().Next(2) == 0;
+                //jogador1 = new Jogador(Context.ConnectionId.ToString(), baralho, valorAleatorio, sala.ToString());
 
             }
+
             
         }
         finally
