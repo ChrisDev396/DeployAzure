29fa329e46c811e96de03dbcdc47fb592da1e3a4
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-23 05:01:35 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -15,14 +15,14 @@ public class PartidaHub : Hub
     private static int sala = 1;
     private static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
-    private static Jogador jogador1;
-    private static Jogador jogador2;
+    //private static Jogador jogador1;
+    //private static Jogador jogador2;
 
-    private static Dictionary<string, List<Jogador>> dictionary;// = new Dictionary<string, List<Jogador>>();
+    //private static Dictionary<string, List<Jogador>> dictionary;// = new Dictionary<string, List<Jogador>>();
 
-    private static List<Jogador> list; //= new List<Jogador>();
+    //private static List<Jogador> list; //= new List<Jogador>();
 
-    private static bool valorAleatorio;// = false;
+    //private static bool valorAleatorio;// = false;
 
     public async Task JoinRoom(string[] baralho)
     {
@@ -46,16 +46,15 @@ public class PartidaHub : Hub
             {
                 //valorAleatorio = !valorAleatorio;
                 //jogador2 = new Jogador(emailClaim, baralho, valorAleatorio, sala.ToString());
-                
+
                 //list.Add(jogador1);
                 //list.Add(jogador2);
                 //dictionary.Add(sala.ToString(), list);
                 //list.Clear();
-
-                
-
+                await Clients.Group(sala.ToString()).SendAsync("JoinRoom", sala.ToString());
                 sala++;
                 usersInRoom = 0;
+                
             }
             else
             {
@@ -63,7 +62,7 @@ public class PartidaHub : Hub
                 //jogador1 = new Jogador(emailClaim, baralho, valorAleatorio, sala.ToString());
                 
             }
-            await Clients.Group(sala.ToString()).SendAsync("JoinRoom", sala.ToString());
+            
         }
         finally
         {
