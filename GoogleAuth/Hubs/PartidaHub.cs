f59c7ec242e5e9a628b8c3de6f83d5c2b59b1dd7
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-25 18:50:43 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -33,8 +33,8 @@ public class PartidaHub : Hub
             var claims = Context.User.Claims;
             var emailClaim = claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email);
 
-            if (emailClaim.Value != null)
-            {}
+            //if (emailClaim.Value != null)
+            //{}
 
             usersInRoom++;
 
