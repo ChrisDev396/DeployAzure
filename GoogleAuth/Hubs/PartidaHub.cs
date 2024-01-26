c5b04636d84ba126fcf110a725fb0a0c0cd59605
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-25 22:34:18 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -49,7 +49,7 @@ public class PartidaHub : Hub
                 list.Add(jogador2);
                 dictionary.Add(sala.ToString(), list);
 
-                await Clients.Group(sala.ToString()).SendAsync("JoinRoom", dictionary[sala.ToString()][0].nome + dictionary[sala.ToString()][0].nome+baralho[0]);
+                await Clients.Group(sala.ToString()).SendAsync("JoinRoom", dictionary[sala.ToString()][0].nome + dictionary[sala.ToString()][1].nome+baralho[0]);
 
                 sala++;
                 usersInRoom = 0;
