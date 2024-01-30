6b507580e9f430ecca211f922346329e76fd51c4
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-29 21:23:38 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -80,7 +80,7 @@ public class PartidaHub : Hub
             //dictionary[roomName][1].vida -= dictionary[roomName][0].forca;
             //dictionary[roomName][0].vida -= dictionary[roomName][1].forca;
 
-            string[] jogadorInfo1 = { dictionary[roomName][0].nome, dictionary[roomName][0].forca.ToString(), dictionary[roomName][0].vida.ToString(),dictionary[roomName][0].heroi, dictionary[roomName][0].turno.ToString() };
+            string[] jogadorInfo1 = { dictionary[roomName][0].nome, dictionary[roomName][0].forca.ToString(), dictionary[roomName][0].vida.ToString(), dictionary[roomName][0].heroi, dictionary[roomName][0].turno.ToString() };
             string[] jogadorInfo2 = { dictionary[roomName][1].nome, dictionary[roomName][1].forca.ToString(), dictionary[roomName][1].vida.ToString(), dictionary[roomName][1].heroi, dictionary[roomName][1].turno.ToString() };
             await Clients.Group(roomName).SendAsync("GetJogadoresStatus", jogadorInfo1.ToList(), jogadorInfo2.ToList());
         }
