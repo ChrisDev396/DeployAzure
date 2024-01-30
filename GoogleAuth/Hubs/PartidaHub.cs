73993c10dc4db8182cc7365e1b0a56edf4b26507
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-01-30 01:52:51 -0300

    Update PartidaHub.cs

--- GoogleAuth/Hubs/PartidaHub.cs
+++ GoogleAuth/Hubs/PartidaHub.cs
@@ -90,12 +90,10 @@ public class PartidaHub : Hub
             dictionary[roomName][1].turno = !dictionary[roomName][1].turno;
             dictionary[roomName][0].turno = !dictionary[roomName][0].turno;
 
-            //dictionary[roomName][0].vida -= dictionary[roomName][1].forca;
-            //dictionary[roomName][1].vida -= dictionary[roomName][0].forca;
 
-            string[] jogadorInfo1 = { dictionary[roomName][0].nome, dictionary[roomName][0].forca.ToString(), dictionary[roomName][0].vida.ToString(), dictionary[roomName][0].heroi, dictionary[roomName][0].turno.ToString() };
+            string jogadorInfo1 = dictionary[roomName][0].nome + "/" + dictionary[roomName][0].forca.ToString() + "/" + dictionary[roomName][0].vida.ToString() + "/" + dictionary[roomName][0].heroi + "/" + dictionary[roomName][0].turno.ToString() ;
+            string jogadorInfo2 = dictionary[roomName][1].nome + "/" + dictionary[roomName][1].forca.ToString() + "/" + dictionary[roomName][1].vida.ToString() + "/" + dictionary[roomName][1].heroi + "/" + dictionary[roomName][1].turno.ToString() ;
 
-            string[] jogadorInfo2 = { dictionary[roomName][1].nome, dictionary[roomName][1].forca.ToString(), dictionary[roomName][1].vida.ToString(), dictionary[roomName][1].heroi, dictionary[roomName][1].turno.ToString() };
             await Clients.Group(roomName).SendAsync("GetJogadoresStatus", jogadorInfo1,jogadorInfo2);
         }
 
