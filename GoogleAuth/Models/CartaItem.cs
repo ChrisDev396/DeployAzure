bc51afbd167ff6bc7ba1667ffe44b3791eb29dce
ChrisDev396 <69995444+ChrisDev396@users.noreply.github.com>
2024-02-03 02:04:49 -0300

    aaa

--- GoogleAuth/Models/CartaItem.cs
+++ GoogleAuth/Models/CartaItem.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 public class CartaItem
 {
-	public static void Item(string carta, Jogador atacante, Jogador alvo)
+
+	public int vida = 0;
+	public int forca = 0;
+	public string tipo = "passiva";//ativo//passiva-ativo
+
+    public static void Item(string carta, Jogador atacante, Jogador alvo)
 	{
 		if(carta == "item1")
 		{
@@ -11,6 +16,11 @@ public class CartaItem
 		else if (carta == "item2")
 		{
             alvo.vida -= atacante.forca;
+		}
+		else if (carta == "item3")
+		{
+			atacante.countJogadas += 1;
+			atacante.itemStatus.Add(new ItemStatus(carta));
 		}
 	}
 }
